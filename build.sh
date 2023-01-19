5181c4a14952b028e028d2902fb1049ddfa17b6c
jlcastro <joselucianocastro10@gmail.com>
2023-01-19 01:28:52 +0100

    modificado estaticos desde BBDD

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py createsuperuser --username --noinput
+python manage.py createsuperuser --username USERNAME --noinput
 python manage.py collectstatic --no-input
 python manage.py migrate
