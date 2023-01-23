074f1c81a92df45747b52c95ba8c9b860a4eb591
jlcastro <joselucianocastro10@gmail.com>
2023-01-23 23:49:14 +0100

    volcado de datos en BBDD

--- build.sh
+++ build.sh
@@ -7,4 +7,8 @@ pip install -r requirements.txt
 python manage.py createsuperuser --no-input
 #python manage.py createsuperuser --username admin 
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate 
+
+python manage.py loaddata prueba.json
+
+python manage.py tailwind build 
